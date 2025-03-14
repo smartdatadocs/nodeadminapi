a9c880180849027cb43e31c633c66860c96cbc35
Anil SIkhram <anilwork13@gmail.com>
2025-03-14 16:29:24 -0400

    added api to onBoard Mongo DB cluster

--- src/app.js
+++ src/app.js
@@ -3,9 +3,14 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const dataRoutes = require('./routes/dataRoutes');
 const metadataRoutes = require('./routes/metadataRoutes');
+const onboardRoute = require('./routes/onboard');
+
 
 
 dotenv.config();
+console.log('ATLAS_PUBLIC_KEY:', process.env.ATLAS_PUBLIC_KEY);
+console.log('ATLAS_PRIVATE_KEY:', process.env.ATLAS_PRIVATE_KEY);
+console.log('ATLAS_PROJECT_ID:', process.env.ATLAS_PROJECT_ID);
 
 const app = express();
 
@@ -18,6 +23,7 @@ app.use(express.json());
 // Routes
 app.use('/api', dataRoutes);
 app.use('/api/metadata', metadataRoutes);
+app.use('/onboard', onboardRoute);
 
 
 // Start the server
